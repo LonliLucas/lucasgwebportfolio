3148a51e55cd2d176778f6ef57245367e71265cf
LonliLucas <lukej.gilless@gmail.com>
2023-10-12 10:31:40 -0600

    Added Movie Knight project link.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var movieKnight = document.querySelector(".movieKnight");
+
+movieKnight.addEventListener("click", function(){
+    window.open("https://cpulsipher24.github.io/Movie-Knight/index.html", "_blank");
+});
